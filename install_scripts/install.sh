b014e71251f746073bf338e54f167fd20a5e31fa
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2025-01-01 17:26:36 +0900

    .

--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -29,7 +29,9 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-cat .zshrc >> ${ZDOTDIR:-$HOME}/.zshrc
+if [ -n '.zshrc' ]; then
+    cat .zshrc >> ${ZDOTDIR:-$HOME}/.zshrc
+fi
 
 echo "prezto installed!"
 
