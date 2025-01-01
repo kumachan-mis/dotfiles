4463bff5232d32456b646aff47aeaeebd11d3b2a
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2025-01-01 12:37:00 +0900

    remove mas install

--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -6,7 +6,7 @@ BASE_DIR=$(cd $(dirname "$0")/../ && pwd)
 #     Dotfiles     #
 ####################
 
-echo "step 1/5: creating symbolic links to dotfiles ..."
+echo "step 1/4: creating symbolic links to dotfiles ..."
 
 dotfiles=$(ls ${BASE_DIR}/dotfiles)
 for dotfile in ${dotfiles}
@@ -22,7 +22,7 @@ echo "symbolic links created!"
 #     Command line tools for Xcode     #
 ########################################
 
-echo "step 2/5: installing command line tools ..."
+echo "step 2/4: installing command line tools ..."
 
 sudo rm -rf /Library/Developer/CommandLineTools
 xcode-select --install
@@ -33,7 +33,7 @@ echo "command line tools installed!"
 #     Homebrew     #
 ####################
 
-echo "step 3/5: installing homebrew ..."
+echo "step 3/4: installing homebrew ..."
 which brew 2>/dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo mkdir -p /usr/local/Frameworks
 sudo chown -R $(whoami):admin /usr/local/Frameworks
@@ -77,7 +77,7 @@ echo "brew installed!"
 #    Python Pip3     #
 ######################
 
-echo "step 4/5: installing pip3 ..."
+echo "step 4/4: installing pip3 ..."
 
 echo "run pip3 install ..."
 
@@ -87,19 +87,3 @@ do
 done < ${BASE_DIR}/apps/pip3_packages.txt
 
 echo "pip3 installed!"
-
-
-#############################
-#     Apps in App Store     #
-#############################
-
-echo "step 5/5: installing apps in App Store ..."
-
-echo "run mas install app ..."
-
-while read app
-do
-    mas install ${app}
-done < ${BASE_DIR}/apps/appstore_apps.txt
-
-echo "apps in App Store installed!"
